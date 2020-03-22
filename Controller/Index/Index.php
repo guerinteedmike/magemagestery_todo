7f0ceaadecb8183814b7b8625a94ff8322a45a43
guerinteedmike <guerinteedmike@gmail.com>
2020-03-22 19:14:03 -0400

    Lesson 4 - Add index controller

---
+++ Controller/Index/Index.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Guerinteed\Todo\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\ResultFactory;
+
+/**
+ * TODO index action.
+ *
+ * @package Guerinteed\Todo\Controller\Index
+ */
+class Index extends Action
+{
+
+    /**
+     * TODO index action.
+     *
+     * @return ResultFactory
+     */
+    public function execute()
+    {
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+    }
+}
