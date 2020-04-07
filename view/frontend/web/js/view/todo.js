70e52afe6d59a5ed3cc07f3ea24bb614e982c639
guerinteedmike <guerinteedmike@gmail.com>
2020-04-07 07:48:34 -0400

    Lesson 15 - component configuration in layout file

--- view/frontend/web/js/view/todo.js
+++ view/frontend/web/js/view/todo.js
@@ -2,5 +2,14 @@ define(['uiComponent'], function (Component) {
     'use strict';
 
     console.log("Mikeg Was Here!!");
-    return Component.extend({});
+    return Component.extend({
+        defaults: {
+            tasks: [
+                {label: "Task 1"},
+                {label: "Task 2"},
+                {label: "Task 3"},
+                {label: "Task 4"},
+            ]
+        }
+    });
 });
